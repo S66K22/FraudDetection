02da9f7d44a0f89dceed9d1f78ee46ee224f33a8
saeed <kermoonshahi@outlook.com>
2026-08-16 13:29:36 +0330

    remove some configs because of lack of memory

--- src/train_rf.py
+++ src/train_rf.py
@@ -16,17 +16,19 @@ logging.basicConfig(
 # Read dataset
 data = np.load("data/preprocessed_data.npz")
 X_train_scaled = data["X_train_scaled"]
+X_test_scaled = data["X_test_scaled"]
 X_train_time_scaled_iso_score = data["X_train_time_scaled_iso_score"]
+X_test_time_scaled_iso_score = data["X_test_time_scaled_iso_score"]
 y_train = data["y_train"]
+y_test = data["y_test"]
 
 # Train Random Forest
 rf_param_grid = {
-    "n_estimators": [100, 200, 400],
-    "criterion": ["gini", "entropy"],
-    "max_depth": [10, 20, 30, None],
-    "min_samples_split": [2, 5, 10, 20],
-    "min_samples_leaf": [1, 2, 5, 10],
-    "max_features": ["sqrt", "log2", None],
+    "n_estimators": [100, 200],
+    "max_depth": [10, 20, 30],
+    "min_samples_split": [2, 5, 10],
+    "min_samples_leaf": [1, 2, 5],
+    "max_features": ["sqrt", None],
     "class_weight": [
         None,
         "balanced",
@@ -40,7 +42,7 @@ rf_param_grid = {
 }
 
 rf_clf_grid_search_1 = train.train(
-    RandomForestClassifier(random_state=42, n_jobs=-1),
+    RandomForestClassifier(random_state=42, n_jobs=2),
     "rf1",
     rf_param_grid,
     train.scoring,
@@ -54,13 +56,13 @@ rf_clf_grid_search_1 = train.train(
 
 train.plot_cm(
     rf_clf_grid_search_1.best_estimator_,
-    X_train_time_scaled_iso_score,
-    y_train,
+    X_test_scaled,
+    y_test,
     "reports/rf_cm1.png",
 )
 
 rf_clf_grid_search_2 = train.train(
-    RandomForestClassifier(random_state=42, n_jobs=-1),
+    RandomForestClassifier(random_state=42, n_jobs=2),
     "rf2",
     rf_param_grid,
     train.scoring,
@@ -74,7 +76,7 @@ rf_clf_grid_search_2 = train.train(
 
 train.plot_cm(
     rf_clf_grid_search_2.best_estimator_,
-    X_train_time_scaled_iso_score,
-    y_train,
+    X_test_time_scaled_iso_score,
+    y_test,
     "reports/rf_cm2.png",
 )
